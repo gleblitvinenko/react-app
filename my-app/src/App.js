449c01ef416420b2002fd356186c62f1ee77d47d
Glieb <gleblitvinenko3007@gmail.com>
2023-01-13 14:44:54 +0200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className='App'>
+      <ul>
+        <li>
+
+
+        </li>
+        <li></li>
+      </ul>
+    </div>
+  );
+
+}
+
+export default App;
